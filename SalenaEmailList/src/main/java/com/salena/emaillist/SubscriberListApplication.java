e0c2480f6f3bcaf31d10f132e7a77ebaf1fd045a
salenastamp <73411319+salenastamp@users.noreply.github.com>
2020-11-22 16:13:57 -0500

    Add files via upload

--- SalenaEmailList/src/main/java/com/salena/emaillist/SubscriberListApplication.java
+++ SalenaEmailList/src/main/java/com/salena/emaillist/SubscriberListApplication.java
@@ -2,9 +2,66 @@ package com.salena.emaillist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.salena.emaillist.subscriber.Subscriber;
+import com.salena.emaillist.subscriber.SubscriberRepository;
+
+
 @SpringBootApplication
 public class SubscriberListApplication {
+	private static final Logger log = LoggerFactory.getLogger(SubscriberListApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriberListApplication.class, args);
+		
 	}
-}
+	@Bean
+	public CommandLineRunner demo(SubscriberRepository repository) {
+		return (args) -> {
+		log.info("Subscribers found with FindAll():");
+		log.info("----------------------------------");
+		for (Subscriber subscriber : repository.findAll()) {
+			log.info(subscriber.toString());
+		}
+		log.info("");
+		
+		repository.findById(7L)
+			.ifPresent(subscriber -> {
+				log.info("Subscriber found with findbyId(1L):");
+				log.info("----------------------------------");
+				log.info(subscriber.toString());
+				log.info("");
+				
+			});
+		log.info("Customer found with findByName('Wobbay'):");
+		log.info("--------------------------------------------");
+		repository.findByName("Wobbay").forEach(bauer -> {
+			log.info(bauer.toString());
+		});
+		
+		log.info("");
+		
+		log.info("Customer found with findByEmail('kassie@squiggle.com'):");
+		log.info("--------------------------------------------");
+		repository.findByEmail("kassie@squiggle.com").forEach(bauer -> {
+			log.info(bauer.toString());
+		
+	});
+		log.info("");
+		
+		log.info("Customer found with findByNameLike('R%'):");
+		log.info("--------------------------------------------");
+		repository.findByNameLike("R%").forEach(bauer -> {
+			log.info(bauer.toString());
+		});
+		};
+}}
--- SalenaEmailList/src/main/java/com/salena/emaillist/subscriber/SubscriberRepository.java
+++ SalenaEmailList/src/main/java/com/salena/emaillist/subscriber/SubscriberRepository.java
@@ -1,7 +1,15 @@
 package com.salena.emaillist.subscriber;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import com.salena.emaillist.subscriber.Subscriber;
 
 public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
 
-}
+	List<Subscriber> findByName(String name);
+	
+	List<Subscriber> findByEmail(String email);
+		
+	List<Subscriber> findByNameLike(String name);
+	
+		}
